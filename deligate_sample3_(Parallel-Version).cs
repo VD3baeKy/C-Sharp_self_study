c1616733bd75cef5ff818c508e4b1fc0e4bcc7f8
VD3baeKy <digfehc@gmail.com>
2025-03-17 23:55:08 +0900

    Create deligate_sample3_(Parallel-Version).cs

---
+++ deligate_sample3_(Parallel-Version).cs
@@ -0,0 +1,52 @@
+/* [cs] */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleEx702
+{
+    class Program
+    {
+        //  デリゲートの宣言
+        delegate void Action(int a);
+        
+        //  1つ目の処理
+        static void Func1(int a)
+        {
+            Console.WriteLine("a={0}", a);
+        }
+        
+        //  2つ目の処理
+        static void Func2(int a)
+        {
+            Console.WriteLine("a*2={0}", a * 2);
+        }
+        
+        //  3つ目の処理
+        static void Func3(int a)
+        {
+            Console.WriteLine("a*3={0}", a * 3);
+        }
+        
+        static void Main(string[] args)
+        {
+            //  デリゲートaの作成
+            Action a = new Action(Func1);
+            //  処理の追加
+            a += new Action(Func2);
+            a += new Action(Func3);
+            
+            //  処理の実行を並列で行う
+            List<Task> tasks = new List<Task>();
+            foreach (Action action in a.GetInvocationList())
+            {
+                tasks.Add(Task.Run(() => action(3)));
+            }
+            
+            //  全てのタスクが完了するのを待つ
+            Task.WaitAll(tasks.ToArray());
+        }
+    }
+}
